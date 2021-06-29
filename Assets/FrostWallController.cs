661852c4b8373650b34c65b63ed3df30e67cbd41
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-06-29 09:42:35 +0900

    壁がプレイヤーの攻撃で破壊されたとき近くにいる敵にダメージを与える処理を追加した

--- Assets/FrostWallController.cs
+++ Assets/FrostWallController.cs
@@ -6,6 +6,8 @@ public class FrostWallController : MonoBehaviour
 {
     /// <summary> 破壊するときに発生する攻撃エフェクト/// </summary>
     [SerializeField] GameObject m_effect;
+    /// <summary> 破壊するときに発生する攻撃コライダー/// </summary>
+    [SerializeField] GameObject m_attackCollider = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +25,22 @@ public class FrostWallController : MonoBehaviour
         if (other.tag == "AttackCollider")
         {
             Instantiate(m_effect, this.transform.position,this.transform.rotation);
-            Destroy(this.gameObject);
+            m_attackCollider.SetActive(true);
+            StartCoroutine(SetNonActive());
+            Destroy(this.gameObject,1);
         }
     }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+    IEnumerator SetNonActive()
+    {
+        m_attackCollider.SetActive(true);
+        yield return new WaitForSeconds(1);
+        m_attackCollider.SetActive(false);
+    }
 }
+
