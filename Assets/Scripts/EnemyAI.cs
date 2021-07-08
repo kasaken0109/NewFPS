de620055bcea086d9684f57251c651ab9a1d9b05
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-08 23:52:54 +0900

    EnemyAiクラスのBaseクラスを仮作成した

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent m_navMeshAgent = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 敵の行動種類
+    /// </summary>
+    public enum ActionType
+    {
+        PATROL,//巡回
+        CHASE,//追跡
+        ATTACK,//攻撃
+    } 
+}
