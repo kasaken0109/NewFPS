c89bf7f23423e96b81d2df47f42079f5b9883f33
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-19 12:53:51 +0900

    StateBaseクラスを継承したAttackState、IdleStateクラスを作製した

---
+++ Assets/Scripts/IdleState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : StateBase
+{
+    // Start is called before the first frame update
+    [SerializeField] StateBase _attackState;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_opponentTag == "") return;
+        if (collision.gameObject == null) return;
+        if (collision.gameObject.CompareTag(_opponentTag))
+        {
+            Debug.Log(collision.gameObject.tag);
+            _actionCtrl.SetCurrent(_attackState);
+        }
+    }
+}
