1b9d330c90c4ef25e2e4e918bbf3da65a82c1384
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-19 12:49:37 +0900

    State遷移を管理するクラスを作製した

---
+++ Assets/Scripts/ActionCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionCtrl
+{
+    StateBase _currentState = null;
+
+    public void SetCurrent(StateBase s)
+    {
+        if (s == null) return;
+        if (_currentState != null)
+        {
+            _currentState.EventCall(StateBase.Event.Leave);
+        }
+
+        _currentState = s;
+        _currentState.SetCtrl(this);
+        _currentState.EventCall(StateBase.Event.Enter);
+    }
+}
