f16702e5b2e7ae87bcc914b48303f1d97df241be
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-20 20:30:34 +0900

    IdleStateが正しく動作するように修正した

--- Assets/Scripts/IdleState.cs
+++ Assets/Scripts/IdleState.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class IdleState : StateBase
 {
     // Start is called before the first frame update
-    [SerializeField] StateBase _attackState;
+    [SerializeField]StateBase _attackState;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,6 +14,16 @@ public class IdleState : StateBase
         if (collision.gameObject.CompareTag(_opponentTag))
         {
             Debug.Log(collision.gameObject.tag);
+            _actionCtrl.SetCurrent(_attackState);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_opponentTag == "") return;
+        if (other.gameObject == null) return;
+        if (other.gameObject.CompareTag(_opponentTag))
+        {
             _actionCtrl.SetCurrent(_attackState);
         }
     }
