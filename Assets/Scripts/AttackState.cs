bce7f1f92779101e31c0214b02a88cce9f4b59ed
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-20 20:28:43 +0900

    AttackStateが正しく動作するように修正した

--- Assets/Scripts/AttackState.cs
+++ Assets/Scripts/AttackState.cs
@@ -19,10 +19,10 @@ public class AttackState : StateBase
     {
         Debug.Log("EnterCallback");
         _animator.SetBool("Attack", true);
-        //if (_opponentTag == "Player")
-        //{
-        //    this.transform.LookAt(GameManager.Player.transform.position);
-        //}
+        if (_opponentTag == "Player")
+        {
+            this.transform.LookAt(GameManager.Player.transform.position);
+        }
         return 0;
     }
 
@@ -38,6 +38,16 @@ public class AttackState : StateBase
         if (_opponentTag == "") return;
         if (collision.gameObject == null) return;
         if (collision.gameObject.CompareTag(_opponentTag))
+        {
+            _actionCtrl.SetCurrent(_idleState);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_opponentTag == "") return;
+        if (other.gameObject == null) return;
+        if (other.gameObject.CompareTag(_opponentTag))
         {
             _actionCtrl.SetCurrent(_idleState);
         }
