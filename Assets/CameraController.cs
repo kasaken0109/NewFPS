ec0b881a99290d5dce537ad2a7abcb1dffa5f563
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-29 18:43:37 +0900

    カメラが視界範囲外に動いたとき自動的に範囲内に戻るように処理を追加した

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform m_pivot = null;
-    [SerializeField]Transform temp = null;
+    [SerializeField] Transform m_UpPivot = null;
+    [SerializeField] Transform m_DownPivot = null;
+    [SerializeField]Vector3 m_temp;
     GameObject m_player;
     //カメラ上下移動の最大、最小角度です。Inspectorウィンドウから設定してください
     [Range(-0.999f, -0.1f)]
@@ -15,8 +17,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_player = GameManager.Player.gameObject;
-        temp.position = new Vector3(0, 2f, 0);
+        m_player = GameManager.Player.gameObject; 
     }
 
     // Update is called once per frame
@@ -25,21 +26,28 @@ public class CameraController : MonoBehaviour
         float hInput = Input.GetAxisRaw("Mouse X");
         float vInput = Input.GetAxisRaw("Mouse Y");
         m_player.transform.Rotate(0, hInput, 0);
-        if (m_player.transform.position.y + 3 > Camera.main.transform.position.y && m_player.transform.position.y - 3 < Camera.main.transform.position.y)
+        if (m_player.transform.position.y + 5 > Camera.main.transform.position.y && m_player.transform.position.y - 3 < Camera.main.transform.position.y)
         {
             //temp = Camera.main.transform;
             Debug.Log("CameraCanMove");
             Camera.main.transform.LookAt(m_pivot, Vector3.up);
             Camera.main.transform.position += new Vector3(0, -vInput * Time.deltaTime * 3, 0);
             //Debug.Log(Camera.main.transform.position);
-            Debug.Log(temp.position);
         }
         else
         {
-            Camera.main.transform.LookAt(temp, Vector3.up);
-            Camera.main.transform.position = temp.position;
+            if (m_player.transform.position.y + 3 <= Camera.main.transform.position.y)
+            {
+                Debug.Log("Up");
+                Camera.main.transform.LookAt(m_pivot, Vector3.up);
+                Camera.main.transform.position = m_UpPivot.position;
+            }
+            else
+            {
+                Debug.Log("Down");
+                Camera.main.transform.LookAt(m_pivot, Vector3.up);
+                Camera.main.transform.position = m_DownPivot.position;
+            }
         }
-
-
     }
 }
