bd5bd1fd72485bb434ff5ee851ed913314e1e1e7
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-07-29 22:03:33 +0900

    コメントを追加した

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// プレイヤーのカメラ移動を処理する
+/// </summary>
 public class CameraController : MonoBehaviour
 {
+    /// <summary>カメラの回転する中心点</summary>
     [SerializeField] Transform m_pivot = null;
-    [SerializeField] Transform m_UpPivot = null;
-    [SerializeField] Transform m_DownPivot = null;
-    [SerializeField]Vector3 m_temp;
+    /// <summary>上の修正点 </summary>
+    [SerializeField] Transform m_UpPoint = null;
+    /// <summary>下の修正点 </summary>
+    [SerializeField] Transform m_DownPoint = null;
     GameObject m_player;
-    //カメラ上下移動の最大、最小角度です。Inspectorウィンドウから設定してください
-    [Range(-0.999f, -0.1f)]
-    public float maxYAngle = -0.5f;
-    [Range(0.1f, 0.999f)]
-    public float minYAngle = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,25 +29,20 @@ public class CameraController : MonoBehaviour
         m_player.transform.Rotate(0, hInput, 0);
         if (m_player.transform.position.y + 5 > Camera.main.transform.position.y && m_player.transform.position.y - 3 < Camera.main.transform.position.y)
         {
-            //temp = Camera.main.transform;
-            Debug.Log("CameraCanMove");
             Camera.main.transform.LookAt(m_pivot, Vector3.up);
             Camera.main.transform.position += new Vector3(0, -vInput * Time.deltaTime * 3, 0);
-            //Debug.Log(Camera.main.transform.position);
         }
         else
         {
             if (m_player.transform.position.y + 3 <= Camera.main.transform.position.y)
             {
-                Debug.Log("Up");
                 Camera.main.transform.LookAt(m_pivot, Vector3.up);
-                Camera.main.transform.position = m_UpPivot.position;
+                Camera.main.transform.position = m_UpPoint.position;
             }
             else
             {
-                Debug.Log("Down");
                 Camera.main.transform.LookAt(m_pivot, Vector3.up);
-                Camera.main.transform.position = m_DownPivot.position;
+                Camera.main.transform.position = m_DownPoint.position;
             }
         }
     }
