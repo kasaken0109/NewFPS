2f013f2e7f6ca487b2c964366111bca6acb0409c
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-08-26 17:59:01 +0900

    時間のインターフェイスIStartTimeとIStopTimeを作成した

---
+++ Assets/Scripts/ITime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ITime : MonoBehaviour
+{
+    interface IStopTime
+    {
+        void StopTime();
+    }
+
+    // Update is called once per frame
+    interface IStartTime
+    {
+        void StartTime();
+    }
+}
