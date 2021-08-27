9cd78e050036fe44fc32e59475d5b29a7bdd20e5
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-08-27 23:09:19 +0900

    GameManagerに咆哮中にPlayerの移動入力を受け付けないように処理を追加した

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
-    public PlayerManager _player;
+    public PlayerManager m_player;
     [SerializeField] GameObject m_bossEnemy = null;
     [SerializeField] Transform m_bossSpawn = null;
 
-    public static GameManager _instance = null;
-    static public PlayerManager Player => _instance._player;
+    public static GameManager Instance = null;
+    static public PlayerManager Player => Instance.m_player;
 
     //適当なのでちゃんとしたシングルトンではない
     void Awake()
     {
-        //_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
-        _instance = this;
+        Instance = this;
     }
 
     private void Update()
@@ -28,5 +27,15 @@ public class GameManager : MonoBehaviour
     public void ShakeCamera()
     {
         iTween.ShakePosition(Camera.main.gameObject, iTween.Hash("x", 0.5, "y", 0.5, "time", 1));
+        StartCoroutine("SetPlayerCameraInput");
+    }
+
+    IEnumerator SetPlayerCameraInput()
+    {
+        CameraController.Instance.SetMoveActive(false);
+        PlayerControll.Instance.SetMoveActive(false);
+        yield return new WaitForSeconds(1.5f);
+        CameraController.Instance.SetMoveActive(true);
+        PlayerControll.Instance.SetMoveActive(true);
     }
 }
