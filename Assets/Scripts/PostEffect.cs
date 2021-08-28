58ea1c74292d078ebbe6270fda2119e224335ef0
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-08-28 16:49:49 +0900

    モノクロのポストエフェクトをかける処理を追加した

---
+++ Assets/Scripts/PostEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffect : MonoBehaviour
+{
+    [SerializeField] Material m_mono = null;
+    // Start is called before the first frame update
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, m_mono);
+    }
+}
