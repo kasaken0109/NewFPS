642f3e0464d8eb2fd63a225edb59263572e758af
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-08-30 23:51:34 +0900

    MeshCut後のMeshを新しいMeshとして保存するクラスを作成した

---
+++ Assets/Scripts/SaveMeshAssets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+
+public class SaveMeshAssets : MonoBehaviour
+{
+    [MenuItem("APIExamples/SaveAssets")]
+    // Start is called before the first frame update
+    public static void SaveMesh(Mesh mesh)
+    {
+        AssetDatabase.CreateAsset(mesh, "Assets/Mesh/" + mesh.name + ".asset");
+        AssetDatabase.SaveAssets();
+    }
+}
