7e60f108669b462dac37af6800c97f47343218ee
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-02 16:43:01 +0900

    敵のブレス攻撃を防ぐ処理と耐久値が一定以下になったときにマテリアルが変更される処理を追加をした

--- Assets/FrostWallController.cs
+++ Assets/FrostWallController.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrostWallController : MonoBehaviour
+public class FrostWallController : MonoBehaviour,IDamage
 {
     /// <summary> 破壊するときに発生する攻撃エフェクト/// </summary>
     [SerializeField] GameObject m_effect;
     /// <summary> 破壊するときに発生する攻撃コライダー/// </summary>
     [SerializeField] GameObject m_attackCollider = null;
+    /// <summary> 破壊されそうな時のマテリアル/// </summary>
+    [SerializeField] Material m_break;
+    /// <summary> 壁のHP/// </summary>
+    [SerializeField] int m_hp = 2;
     /// <summary> 破壊するFrostWall/// </summary>
     [SerializeField] GameObject m_frostwall = null;
     // Start is called before the first frame update
@@ -19,17 +23,18 @@ public class FrostWallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "AttackCollider")
         {
-            m_frostwall.SetActive(false);
-            Instantiate(m_effect, this.transform.position,this.transform.rotation);
-            StartCoroutine(SetNonActive());
-            Destroy(this.gameObject,1);
+            AddDamage(m_hp);
+        }
+        else if(other.tag == "Enemy")
+        {
+            AddDamage(1);
+            Destroy(other.gameObject);
         }
     }
 
@@ -43,6 +48,24 @@ public class FrostWallController : MonoBehaviour
         m_attackCollider.SetActive(true);
         yield return new WaitForSeconds(1);
         m_attackCollider.SetActive(false);
+    }
+
+    public void AddDamage(int damage)
+    {
+        if(m_hp <= damage)
+        {
+            m_frostwall.SetActive(false);
+            Instantiate(m_effect, this.transform.position, this.transform.rotation);
+            StartCoroutine(SetNonActive());
+            Destroy(this.gameObject, 1);
+        }
+        else
+        {
+            m_hp -= damage;
+            if (m_hp <= 5) GetComponentInChildren<Renderer>().material = m_break;
+        }
+        Debug.Log(m_hp);
+
     }
 }
 
