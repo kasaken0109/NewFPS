7b8db17cd1badc900bc0bf1c425fc5cff6e6c7cb
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-03 15:10:36 +0900

    剣の攻撃を管理するクラスを作成した

---
+++ Assets/Sword.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour,IWeapon
+{
+    [SerializeField] float[] m_activeColliderTime;
+    [SerializeField] GameObject[] m_activeCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NormalAttack()
+    {
+        StartCoroutine(ColliderGenerater.Instance.GenerateCollider(m_activeCollider[0], 1f));
+    }
+
+    public void BasicAttack()
+    {
+        ColliderGenerater.Instance.StartActiveCollider(m_activeCollider[0], 1f);
+    }
+
+    public void SpecialAttack()
+    {
+        ColliderGenerater.Instance.StartActiveCollider(m_activeCollider[1], 1f);
+    }
+}
