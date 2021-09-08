631eb15495b08042f1915107f7787ee5b5769f06
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-08 21:52:10 +0900

    タイトル遷移のクラスを仮作成した

---
+++ Assets/TitleManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] PlayableDirector m_playable;
+    [SerializeField] GameObject[] m_canvasObj = null;
+    [SerializeField] GameObject m_skip = null;
+    [SerializeField] GameObject m_anim = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsDone())
+        {
+            SetShowUI();
+            m_skip?.SetActive(false);
+            Debug.Log("Done");
+        }
+        else
+        {
+            
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("NotDone");
+                m_skip?.SetActive(true);
+            }
+        }
+    }
+
+    public bool IsDone()
+    {
+        return m_playable.time >= m_playable.duration;
+    }
+
+    public void ActiveButton()
+    {
+        m_playable.Stop();
+        SetShowUI();
+    }
+
+    void SetShowUI()
+    {
+        if (m_canvasObj == null) return;
+        foreach (var item in m_canvasObj)
+        {
+            item.SetActive(true);
+        }
+        m_anim?.SetActive(false);
+    }
+}
