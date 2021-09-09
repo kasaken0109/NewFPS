ce6ea57cac8240d70ed2fde1552ef6d8b1a3f252
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-09 23:44:29 +0900

    リロード時の画像表示処理を追加した

--- Assets/Scripts/fire.cs
+++ Assets/Scripts/fire.cs
@@ -14,7 +14,8 @@ public class fire : MonoBehaviour
     /// <summary>一画面の最大段数 (0 = 無制限)</summary>
     [SerializeField, Range(0, 10)] int m_bulletLimit = 0;
     GameObject m_reload = null;
-
+    /// <summary>リロード時間</summary>
+    [SerializeField] float m_seconds = 2f;
     [SerializeField] float m_fireInterval = 0.15f;
     [SerializeField] AudioClip []m_shootSound = null;
     //[SerializeField] AudioClip m_airSound = null;
@@ -26,17 +27,15 @@ public class fire : MonoBehaviour
     [SerializeField] public int m_bulletMaxNum = 4;
     GameObject m_textBox;
     Coroutine m_coroutine;
-    Rigidbody2D m_rb;
     bool IsSpecial = false;
+    bool IsReload = false;
     // Start is called before the first frame update
     void Start()
     {
-        m_rb = GetComponent<Rigidbody2D>();
         m_shootAnim = GetComponent<Animator>();
-        m_textBox = GameObject.Find("BulletText");
-        m_text = m_textBox.GetComponent<Text>();
-        m_reload = GameObject.Find("Reload");
-        m_bulletNum = m_bulletMaxNum;
+        m_bulletNum = PlayerPrefs.GetInt("Bullet2");
+        GameObject.Find("CannonImage").GetComponent<Image>().fillAmount = 1;
+        m_reload = PlayerManager.Instance.m_reloadImage;
         if (m_muzzle == null)
         {
             m_muzzle = GameObject.FindGameObjectWithTag("Muzzle").transform;
@@ -44,10 +43,20 @@ public class fire : MonoBehaviour
         //m_reload.SetActive(false);
     }
 
+    private void Awake()
+    {
+        m_textBox = PlayerManager.Instance.m_textBox2;
+        m_text = m_textBox.GetComponent<Text>();
+        m_bulletNum = PlayerPrefs.GetInt("Bullet2");
+        m_text.text = m_bulletNum +"/" + m_bulletMaxNum;
+        
+        if (m_bulletNum == 0) m_reload?.SetActive(true);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        m_text.text = m_bulletNum + "/" + m_bulletMaxNum;
+        if(!IsReload)m_text.text = m_bulletNum + "/" + m_bulletMaxNum;
 
         if (Input.GetButtonDown("Fire1"))
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             {
@@ -63,11 +72,11 @@ public class fire : MonoBehaviour
                     StartCoroutine(nameof(BigWall));
                 }
             }
-            else
+            else if(!IsReload)
             {
                 AudioSource.PlayClipAtPoint(m_shootSound[1], m_muzzle.position,0.1f);
                 Debug.Log("リロードしてください");
-                //m_reload.SetActive(true);
+                m_reload.SetActive(true);
 
             }
         }
@@ -84,12 +93,21 @@ public class fire : MonoBehaviour
                 StopCoroutine(nameof(BigWall));
             }
         }
-        if (Input.GetButtonDown("Reload"))
+        if (Input.GetButtonDown("Reload") && !IsReload)
         {
-            AudioSource.PlayClipAtPoint(m_shootSound[2], m_muzzle.position);
+            AudioSource.PlayClipAtPoint(m_shootSound[2], this.transform.position);
+            Debug.Log("Reload");
             Reload();
-            //m_reload.SetActive(false);
         }
+    }
+
+    void OnDestroy()
+    {
+        PlayerPrefs.SetInt("Bullet2", m_bulletNum);
+        Debug.Log(m_bulletNum);
+        PlayerPrefs.Save();
+        StopCoroutine(nameof(WaitSeconds));
+        m_text.text = m_bulletNum + "/" + m_bulletMaxNum;
     }
 
     void PlayShootSound()
@@ -131,10 +149,28 @@ public class fire : MonoBehaviour
         go = Instantiate(m_bulletPrefab, m_muzzle.position, m_bulletPrefab.transform.rotation);  // インスペクターから設定した m_bulletPrefab をインスタンス化する
     }
 
-    public void Reload()
+    void Reload()
     {
-        Debug.Log("リロード中");
+        m_text.text = "リロード中";
+        StopCoroutine(nameof(WaitSeconds));
+        StartCoroutine("WaitSeconds");
+    }
+
+    IEnumerator WaitSeconds()
+    {
+        m_reload?.SetActive(false);
+        var i = GameObject.Find("CannonImage").GetComponent<Image>();
+        i.fillAmount = 0;
+        IsReload = true;
+        while (i.fillAmount <= 0.99f)
+        {
+            i.fillAmount += 0.01f;
+            yield return new WaitForSeconds(m_seconds / 100);
+        }
+        //yield return new WaitForSeconds(m_seconds);
         m_bulletNum = m_bulletMaxNum;
+        //m_reload.Play();
+        IsReload = false;
     }
 
 }
