49e527ed533e67a912b45bac5e8856c0e12788ef
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-09 23:54:31 +0900

    Videoを再生するクラスを作成した

---
+++ Assets/VideoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    [SerializeField] VideoPlayer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayVideo(VideoClip videoClip)
+    {
+        player.Pause();
+        player.clip = videoClip;
+        player.Play();
+    }
+}
