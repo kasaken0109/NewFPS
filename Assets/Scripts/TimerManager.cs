2cbbc77af24fb213d15d8549beb69a0ab0168515
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-09 23:52:07 +0900

    ゲーム終了時にクリア時間を保存するように処理を追加した

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -13,10 +13,14 @@ public class TimerManager : MonoBehaviour
     [SerializeField]TextManager textManager;
     Animation m_anim = null;
     GameState m_gameState;
+    int maxTimeLimit;
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.SetInt("MaxTime", (int)timeLimit);
+        PlayerPrefs.Save();
         m_gameClear.SetActive(false);
+        maxTimeLimit = (int)timeLimit;
         m_gameState = GameState.PLAYING;
         m_anim = gameObject.GetComponent<Animation>();
         m_menu = false;
@@ -25,8 +29,22 @@ public class TimerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeLimit -= Time.deltaTime;
         m_text.text = "のこり時間：" + Mathf.CeilToInt(timeLimit);
+        if (GameManager.Instance.GameStatus == GameManager.GameState.PLAYERWIN)
+        {
+            PlayerPrefs.SetInt("TimeScore", maxTimeLimit - Mathf.CeilToInt(timeLimit));
+            PlayerPrefs.Save();
+        }
+        else if (GameManager.Instance.GameStatus == GameManager.GameState.PLAYERLOSE)
+        {
+            PlayerPrefs.SetInt("TimeScore", maxTimeLimit);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            timeLimit -= Time.deltaTime;
+        }
+        
         if (timeLimit <= 10f)
         {
             m_text.color = Color.red;
@@ -37,13 +55,6 @@ public class TimerManager : MonoBehaviour
                 m_gameOver.SetActive(true);
                 m_menu = true;
             }
-        }
-
-        if (timeLimit >= 0 && textManager.enemyFullNum < 0)
-        {
-            Debug.Log(textManager.enemyFullNum);
-            m_gameClear.SetActive(true);
-            m_menu = true;
         }
     }
 
