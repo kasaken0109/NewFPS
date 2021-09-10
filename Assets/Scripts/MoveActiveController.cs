4520f2355bf66d27e1023b738ff7717b7ad76696
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-10 22:08:09 +0900

    エリアに入った際、チュートリアルを表示するクラスを作成した

---
+++ Assets/Scripts/MoveActiveController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveActiveController : MonoBehaviour
+{
+    [SerializeField] int activeNum;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerTutorialControll>().Tutorial(activeNum);
+        }
+    }
+}
