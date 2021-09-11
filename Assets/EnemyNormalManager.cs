8ac67faa279135d11654011b7507c1a56849228a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-12 07:09:28 +0900

    小型モンスターの挙動を管理するクラスを作成した

---
+++ Assets/EnemyNormalManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNormalManager : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent = null;
+    Animator m_animator = null;
+    GameObject m_attack = null;
+    [SerializeField]GameObject m_attackCol = null;
+    float m_timer = 0f;
+    [SerializeField] float m_skillWaitTime = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+        m_animator.SetFloat("Speed",navMeshAgent.velocity.magnitude);
+        Debug.Log($"ss:{navMeshAgent.velocity.magnitude}");
+        if(navMeshAgent.velocity.magnitude <= 0.1f)
+        {
+            m_animator.SetTrigger("Attack");
+        }
+        if (m_attack && m_timer >= 2f)
+        {
+            navMeshAgent.SetDestination(m_attack.gameObject.transform.position);
+            transform.LookAt(m_attack.transform);
+            m_timer = 0f;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            navMeshAgent.SetDestination(other.gameObject.transform.position);
+            m_attack = other.gameObject;
+        }
+    }
+
+    public void ActiveCol()
+    {
+        ColliderGenerater.Instance.StartActiveCollider(m_attackCol, m_skillWaitTime);
+    }
+}
