8a580b987b0c8e9a7d5bfe72d532391365783d69
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-14 12:15:12 +0900

    クエストの情報を保有するScriptableObjectを作成した

---
+++ Assets/Quest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptable/Create QuestData")]
+public class Quest : ScriptableObject
+{
+    [SerializeField] string m_loadSceneName = null;
+    [SerializeField] string m_questName = null;
+    [SerializeField] string m_questText = null;
+    [SerializeField] Sprite m_image = null;
+    // Start is called before the first frame update
+    public void LoadQuest()
+    {
+        SceneLoader.Instance.SceneLoad(m_loadSceneName);
+    }
+}
