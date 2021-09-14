88b19b9e0552985bb2d3f85b3a6b6acceec0e914
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-14 12:36:31 +0900

    Questのプロパティを返す関数を追加した

--- Assets/Quest.cs
+++ Assets/Quest.cs
@@ -9,9 +9,11 @@ public class Quest : ScriptableObject
     [SerializeField] string m_questName = null;
     [SerializeField] string m_questText = null;
     [SerializeField] Sprite m_image = null;
+    public string LoadSceneName() { return m_loadSceneName; }
+    public string QuestName() { return m_questName; }
+    public string QuestText() { return m_questText; }
+
+    public Sprite QuestImage() { return m_image; }
+
     // Start is called before the first frame update
-    public void LoadQuest()
-    {
-        SceneLoader.Instance.SceneLoad(m_loadSceneName);
-    }
 }
