d49576eea543f29d53d1c99b0b7ff4023cd77374
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-14 12:45:13 +0900

    クエストの表示を管理するクラスを作成した

---
+++ Assets/QuestManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    [SerializeField] Quest[] m_quests = null;
+    [SerializeField] QuestPanel[] m_questPanels = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
