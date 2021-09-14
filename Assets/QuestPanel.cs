ce136eaca6cc8fd92afbf5493dce2d2094aa7821
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-14 12:16:00 +0900

    Questの情報を受け取り、表示するクラスを作成した

---
+++ Assets/QuestPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestPanel : MonoBehaviour
+{
+    [SerializeField] Quest m_quest = null;
+    string m_loadSceneName = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadQuest()
+    {
+        SceneLoader.Instance.SceneLoad(m_loadSceneName);
+    }
+}
