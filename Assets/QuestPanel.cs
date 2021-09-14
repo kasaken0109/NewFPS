e51fa72227e1c7fa02f01c972241f31da313692b
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-14 12:37:14 +0900

    アサインされたQuestDataを表示する処理を追加した

--- Assets/QuestPanel.cs
+++ Assets/QuestPanel.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestPanel : MonoBehaviour
 {
     [SerializeField] Quest m_quest = null;
+    [SerializeField] Image m_questImage = null;
+    [SerializeField] Text m_questName = null;
+    [SerializeField] Text m_questText = null;
     string m_loadSceneName = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (!m_quest) Debug.LogError("QuestDataがありません！");
+        m_questImage.sprite = m_quest.QuestImage();
+        m_loadSceneName = m_quest.LoadSceneName();
+        m_questName.text = m_quest.QuestName();
+        m_questText.text = m_quest.QuestText();
     }
 
     public void LoadQuest()
