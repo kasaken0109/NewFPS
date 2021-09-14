a64dd1c73e9c4de56c428b8c4c1ceaefea76095b
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-14 12:13:58 +0900

    Gateが出現した際にZoomするように処理を追加した

--- Assets/GateController.cs
+++ Assets/GateController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GateController : MonoBehaviour
 {
+    [SerializeField] GameObject m_camera = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(nameof(ZoomMe));
     }
 
     // Update is called once per frame
@@ -19,5 +20,13 @@ public class GateController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) SceneLoader.Instance.SceneLoad();
+    }
+
+    IEnumerator ZoomMe()
+    {
+        m_camera?.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        m_camera?.SetActive(false);
+        GameManager.Instance.m_player.gameObject.GetComponent<CameraController>().ResetCamera();
     }
 }
