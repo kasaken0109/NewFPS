0dfb1ca3a41b2aee9d84f8e546e1f2fea7bfc8a6
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-15 23:42:34 +0900

    敵が凍る処理と戦闘に入る前にダメージを与えられない処理を追加した

--- Assets/EnemyNormalController.cs
+++ Assets/EnemyNormalController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyNormalController : MonoBehaviour,IDamage
 {
@@ -9,12 +10,20 @@ public class EnemyNormalController : MonoBehaviour,IDamage
     [SerializeField] GameObject m_attackCol = null;
     [SerializeField] GameObject m_gate = null;
     [SerializeField] float m_skillWaitTime = 0.5f;
+    [SerializeField] int m_mp = 200;
+    [SerializeField] float m_freezeTime = 5f;
+    public GameObject m_froznBody = null;
+    int mp;
+
+    NavMeshAgent navMeshAgent;
     Animator m_anim;
     public void AddDamage(int damage)
     {
+        if (IsNoDamege) return;
         if(damage <= m_hp)
         {
             m_hp -= damage;
+            mp -= (30 - damage);
         }
         else
         {
@@ -25,10 +34,38 @@ public class EnemyNormalController : MonoBehaviour,IDamage
 
     }
 
+    bool IsNoDamege = true;
+    public void SetNoDamege(bool value)
+    {
+        IsNoDamege = value;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        mp = m_mp;
         m_anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(nameof(FrostMode));
+
+    }
+    IEnumerator FrostMode()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (mp <= 0)
+            {
+                navMeshAgent.SetDestination(gameObject.transform.position);
+                m_froznBody.SetActive(true);
+                m_anim.SetBool("IsFreeze", true);
+                yield return new WaitForSeconds(m_freezeTime);
+                m_anim.SetBool("IsFreeze", false);
+                m_froznBody.SetActive(false);
+                navMeshAgent.SetDestination(GameManager.Player.transform.position);
+                mp = m_mp;
+            }
+        }
     }
 
     // Update is called once per frame
