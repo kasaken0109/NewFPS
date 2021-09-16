d520e089a19a07e21ff53e12a79ade1a7ddffef8
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-16 23:32:17 +0900

    敵の強力なブレス攻撃を生成する関数を追加した

--- Assets/Scripts/EnemyContoroller.cs
+++ Assets/Scripts/EnemyContoroller.cs
@@ -8,6 +8,7 @@ public class EnemyContoroller : MonoBehaviour
     Rigidbody m_rb;
     [SerializeField] GameObject attackCollider;
     [SerializeField] GameObject m_breath;
+    [SerializeField] GameObject m_finalBreath;
     [SerializeField] Transform m_spwanBreath;
     [SerializeField] float m_hitTime = 1f;
 
@@ -20,18 +21,28 @@ public class EnemyContoroller : MonoBehaviour
     {
         Debug.Log("Basic");
         //this.transform.DOMove(this.transform.position + this.transform.forward * 2, 0.2f);
-        m_rb.DOMove(this.transform.position + this.transform.forward * 2, 1f);
+        this.transform.DOMove(gameObject.transform.position + gameObject.transform.forward * 2, 1f);
         //m_rb.velocity = this.transform.forward * 100;
     }
 
     public void JumpAttack()
     {
-        m_rb.DOMove(this.transform.position + this.transform.up * 2, 1f);
+        this.transform.DOMove(GameManager.Player.transform.position, 2f);
+    }
+
+    public void SetPosition()
+    {
+        gameObject.transform.position = GameManager.Player.transform.position;
     }
 
     public void BreathAttack()
     {
         Instantiate(m_breath,m_spwanBreath.position, m_spwanBreath.rotation);
+    }
+
+    public void FinalBreathAttack()
+    {
+        Instantiate(m_finalBreath, m_spwanBreath.position, m_spwanBreath.rotation);
     }
 
     public void JumpAttackEffect()
