483b3244147003d712747284bc3534efa4055bd3
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-17 23:16:13 +0900

    ジャンプの処理を変更した

--- Assets/Scripts/PlayerTutorialControll.cs
+++ Assets/Scripts/PlayerTutorialControll.cs
@@ -182,7 +182,7 @@ public class PlayerTutorialControll : ColliderGenerater
         {
             m_anim.SetTrigger("JumpFlag");
             m_rb.useGravity = false;
-            m_rb.AddForce(Vector3.up * m_jumpPower * powerUpRate, ForceMode.Impulse);
+            m_rb.DOMoveY(5, 0.5f);
             m_rb.constraints = RigidbodyConstraints.FreezeRotation;
             m_rb.useGravity = true;
 
