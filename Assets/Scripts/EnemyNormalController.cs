ec2cf58427af1e53b98054da3c699e98f805688c
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-09-27 00:31:19 +0900

    シーンを複製した

---
+++ Assets/Scripts/EnemyNormalController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNormalController : MonoBehaviour,IDamage
+{
+    [SerializeField] int m_hp = 150;
+    [SerializeField] GameObject m_deadBody = null;
+    [SerializeField] GameObject m_attackCol = null;
+    [SerializeField] GameObject m_gate = null;
+    [SerializeField] float m_skillWaitTime = 0.5f;
+    [SerializeField] int m_mp = 200;
+    [SerializeField] float m_freezeTime = 5f;
+    public GameObject m_froznBody = null;
+    int mp;
+
+    NavMeshAgent navMeshAgent;
+    Animator m_anim;
+    public void AddDamage(int damage)
+    {
+        if (IsNoDamege) return;
+        if(damage <= m_hp)
+        {
+            m_hp -= damage;
+            mp -= (30 - damage);
+        }
+        else
+        {
+            Instantiate(m_deadBody, transform.position, transform.rotation);
+            m_gate.SetActive(true);
+            GameManager.Instance.GameStatus = GameManager.GameState.PLAYERWIN;
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    bool IsNoDamege = true;
+    public void SetNoDamege(bool value)
+    {
+        IsNoDamege = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mp = m_mp;
+        m_anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(nameof(FrostMode));
+
+    }
+    IEnumerator FrostMode()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (mp <= 0)
+            {
+                navMeshAgent.SetDestination(gameObject.transform.position);
+                m_froznBody.SetActive(true);
+                m_anim.SetBool("IsFreeze", true);
+                yield return new WaitForSeconds(m_freezeTime);
+                m_anim.SetBool("IsFreeze", false);
+                m_froznBody.SetActive(false);
+                navMeshAgent.SetDestination(GameManager.Player.transform.position);
+                mp = m_mp;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public void ActiveCol()
+    {
+        ColliderGenerater.Instance.StartActiveCollider(m_attackCol, m_skillWaitTime);
+    }
+}
---
+++ Assets/Scripts/GateController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController : MonoBehaviour
+{
+    [SerializeField] GameObject m_camera = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(ZoomMe));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneLoader.Instance.SceneLoad();
+            SoundManager.Instance.PlayMove();
+        }
+    }
+
+    IEnumerator ZoomMe()
+    {
+        m_camera?.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        m_camera?.SetActive(false);
+        GameManager.Instance.m_player.gameObject.GetComponent<CameraController>().ResetCamera();
+    }
+}
