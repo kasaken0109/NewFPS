4cfa20be54acedd654ba5ee2afb5d2a4ad2304a4
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-11-14 22:48:09 +0900

    Summaryを追加した

--- Assets/Scripts/AttackcolliderController.cs
+++ Assets/Scripts/AttackcolliderController.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class AttackcolliderController : MonoBehaviour
 {
-    [SerializeField] int m_attackPower = 15;
-    [SerializeField] GameObject m_hitEffect = null;
-    [SerializeField] AudioClip m_hit;
-    [SerializeField] string m_opponentTagName = "Player";
-    bool CanHit;
-    // Start is called before the first frame update
+    [SerializeField]
+    [Tooltip("攻撃力")]
+    private int m_attackPower = 15;
+
+    [SerializeField]
+    [Tooltip("衝突時に発生するエフェクト")]
+    private GameObject m_hitEffect = null;
+
+    [SerializeField]
+    [Tooltip("ヒットサウンド")]
+    private AudioClip m_hit;
+
+    [SerializeField]
+    [Tooltip("攻撃対象のタグ名")]
+    private string m_opponentTagName = "Player";
+
+    [Tooltip("攻撃が有効かどうか")]
+    private bool CanHit;
+
     private void OnEnable()
     {
+        //コライダーがアクティブになったときに攻撃を有効にする
         CanHit = true;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
         if (collision.gameObject.tag == m_opponentTagName && CanHit)
         {
-            Debug.Log("Direct");
             collision.gameObject.GetComponentInParent<IDamage>().AddDamage(m_attackPower);
             CanHit = false;
         }
@@ -28,19 +40,21 @@ public class AttackcolliderController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
-        if (other.CompareTag("Item"))
-        {
-            other.gameObject.GetComponentInParent<IDamage>().AddDamage(m_attackPower);
-        }
-        //Debug.Log(other.name);
+        if (other.CompareTag("Item")) other.gameObject.GetComponentInParent<IDamage>().AddDamage(m_attackPower);
+        
         if (other.tag == m_opponentTagName && CanHit)
         {
             other.gameObject.GetComponentInParent<IDamage>().AddDamage(m_attackPower);
             if(m_hit)SoundManager.Instance.PlayHit(m_hit,gameObject.transform.position);
-            if (m_hitEffect) Instantiate(m_hitEffect, other.ClosestPoint(transform.position), GameManager.Player.transform.rotation);
+            if(m_hitEffect) Instantiate(m_hitEffect, other.ClosestPoint(transform.position), GameManager.Player.transform.rotation);
             CanHit = false;
         }
     }
+
+    /// <summary>
+    /// 追加ダメージを返す
+    /// </summary>
+    /// <param name="addDamage"></param>
+    /// <returns></returns>
     public int AddDamageCount(int addDamage) { return m_attackPower + addDamage; }
 }
