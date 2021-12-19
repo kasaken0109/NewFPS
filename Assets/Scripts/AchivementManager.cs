95e381268442a0f31c382e1dde78ef2092b63828
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-12-19 23:35:51 +0900

    アチーブメントの処理を実装した

---
+++ Assets/Scripts/AchivementManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchivementManager:MonoBehaviour
+{
+    public static AchivementManager Instance { get; private set; }
+
+    private static int m_dodgeCount = 0;
+
+    public int DodgeCount { get => m_dodgeCount; set { m_dodgeCount = value; }}
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+}
