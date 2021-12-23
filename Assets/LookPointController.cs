da4bad18b058cbad6be58200e6db95dd21b53567
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2021-12-23 23:53:48 +0900

    カメラの姿勢制御のクラスを作成した

---
+++ Assets/LookPointController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// キャラクターの姿勢変更時に見るポイントを変更する
+/// </summary>
+public class LookPointController : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("ズームする距離")]
+    float m_zoomDistance = 0.5f;
+
+    Animator m_anim;
+
+    private void Start()
+    {
+        m_anim = GetComponentInParent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + m_anim.GetFloat("Speed") > 12 ? m_zoomDistance : 0); //m_anim.GetBoneTransform(HumanBodyBones.Neck).position;
+    }
+}
