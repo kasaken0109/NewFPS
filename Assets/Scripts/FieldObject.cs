3e548a268f4facde5e7917f2f1af25a4cebcc646
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-01-23 22:16:27 +0900

    コメントを削除した

---
+++ Assets/Scripts/FieldObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldObject : MonoBehaviour,IDamage
+{
+    [SerializeField]
+    [Tooltip("オブジェクトのHP")]
+    int m_hp = 5;
+    [SerializeField]
+    [Tooltip("破壊時に生成されるオブジェクト")]
+    GameObject m_instance = default;
+    public void AddDamage(int damage)
+    {
+        m_hp = m_hp > damage ? m_hp - damage : 0;
+        if (m_hp == 0) 
+        {
+            Instantiate(m_instance, transform);
+            Destroy(this.gameObject,1);
+        }
+    }
+}
