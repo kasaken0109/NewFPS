e5f6edee16f71f4840a1d5c21c40b4974edf37b3
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-02-12 22:39:00 +0900

    弾の選択をするクラスを作成した

---
+++ Assets/BulletSelectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletSelectController : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_image.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3")) m_image.gameObject.SetActive(true);
+        else if(Input.GetButtonUp("Fire3")) m_image.gameObject.SetActive(false);
+    }
+}
