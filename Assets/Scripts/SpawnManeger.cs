03473c98541c70d6ab9d4195bc8c6e605050d27b
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-02-20 23:04:39 +0900

    サマリーを追加した

--- Assets/Scripts/SpawnManeger.cs
+++ Assets/Scripts/SpawnManeger.cs
@@ -1,23 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// 敵の生成を行う
+/// </summary>
 public class SpawnManeger : MonoBehaviour
 {
-    [SerializeField] Transform [] e_spawnPoint = null;
-    [SerializeField] GameObject enemy = null;
+    [SerializeField]
+    private Transform [] m_spawnPoint = null;
+    [SerializeField]
+    private GameObject enemy = null;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < e_spawnPoint.Length; i++)
+        for (int i = 0; i < m_spawnPoint.Length; i++)
         {
-            enemy = Instantiate<GameObject>(enemy, e_spawnPoint[i].position, e_spawnPoint[i].rotation);
+            Instantiate(enemy, m_spawnPoint[i].position, m_spawnPoint[i].rotation);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
