fce4e3081a03c8aecf2ce2b797a6afc81bc8ca17
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-02-21 23:54:43 +0900

    サマリーを追加した

--- Assets/Scripts/AchivementManager.cs
+++ Assets/Scripts/AchivementManager.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// アクション成功回数を管理する
+/// </summary>
 public class AchivementManager:MonoBehaviour
 {
     public static AchivementManager Instance { get; private set; }
 
+    [Tooltip("回避に成功した回数")]
     private static int m_dodgeCount = 0;
 
+    /// <summary>
+    /// 回避に成功した回数
+    /// </summary>
     public int DodgeCount { get => m_dodgeCount; set { m_dodgeCount = value; }}
     private void Awake()
     {
