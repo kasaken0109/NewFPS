52b68d213349c44baebfe130fdd18a180094ec36
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-02-26 23:37:57 +0900

    弾の選択UIを変更する処理を追加した

--- Assets/BulletSelectController.cs
+++ Assets/BulletSelectController.cs
@@ -6,7 +6,22 @@ using UnityEngine.UI;
 public class BulletSelectController : MonoBehaviour
 {
     [SerializeField]
+    [Tooltip("バレット選択UI")]
     private Image m_image;
+
+    [SerializeField]
+    [Tooltip("")]
+    private Text m_bullet;
+
+    [SerializeField]
+    private BulletFire m_bulletFire = default;
+
+    [SerializeField]
+    private List<Bullet> m_IDs;
+
+    private int bulletIndex = 0;
+
+    private bool isSelected = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +31,18 @@ public class BulletSelectController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire3")) m_image.gameObject.SetActive(true);
-        else if(Input.GetButtonUp("Fire3")) m_image.gameObject.SetActive(false);
+        isSelected = Input.GetButton("Fire3");
+        m_image.gameObject.SetActive(Input.GetButton("Fire3"));
+
+    }
+
+    public void SelectBullet(int bullet)
+    {
+        Bullet temp = default;
+        foreach (var item in m_IDs)
+        {
+            if (item.BulletID == bullet) temp = item;
+        }
+        m_bulletFire.EquipBullet(temp);
     }
 }
