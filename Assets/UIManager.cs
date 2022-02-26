6e1c9e5b11e2cde28d79a8904f36a775528d333a
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-02-26 23:40:27 +0900

    スクリプトの移動を行った

---
+++ Assets/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControllerBase
+{
+    [SerializeField]
+    [Tooltip("表示するUIオブジェクト")]
+    private GameObject m_uiObj;
+
+    /// <summary>
+    /// UIオブジェクトを有効にする
+    /// </summary>
+    public virtual void EnableUI()
+    {
+        m_uiObj.SetActive(true);
+    }
+
+    /// <summary>
+    /// UIオブジェクトを無効にする
+    /// </summary>
+    public virtual void DisableUI()
+    {
+        m_uiObj.SetActive(false);
+    }
+}
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
