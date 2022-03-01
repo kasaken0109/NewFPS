3adcdcbab5ce5f59396da699e2430bd8197f9be4
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-03-01 21:52:36 +0900

    メニューの関数を定義した

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,57 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class MenuController : MonoBehaviour
 {
-    [SerializeField] GameObject menu;
-    GameObject fireScript;
-    bool menuFlag = false;
-    TimerManager timerManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        fireScript = GameObject.FindGameObjectWithTag("Player");
-        fireScript.GetComponent<fire>();
-    }
+    [SerializeField]
+    private GameObject menu = default;
+    [SerializeField]
+    private CinemachineVirtualCamera virtualCamera = default;
 
+    private bool menuFlag = true;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
     // Update is called once per frame
     void Update()
     {
         if (GameManager.Instance.GameStatus == GameManager.GameState.PLAYERWIN || GameManager.Instance.GameStatus == GameManager.GameState.PLAYERLOSE) return;
         if (Input.GetButtonDown("Menu"))
         {
-            if (!menuFlag)
-            {
-                Time.timeScale = 0;
-                menu.SetActive(true);
-                GameManager.Instance.GameStatus = GameManager.GameState.STOP;
-                menuFlag = true;
-
-            }
-            else
-            {
-                Time.timeScale = 1;
-                menu.SetActive(false);
-                GameManager.Instance.GameStatus = GameManager.GameState.PLAYING;
-                menuFlag = false;
-            }
-            
+            SetMenu();
         }
 
         if (!menuFlag)
         {
             Cursor.visible = false;
+            virtualCamera.enabled = true;
             //Cursor.lockState = CursorLockMode.Locked;
         }
         else
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.Confined;
+            virtualCamera.enabled = false;
         }
 
     }
-    
+
+    public void SetMenu()
+    {
+        Time.timeScale = menuFlag ? 1 : 0;
+        menu.SetActive(!menuFlag);
+        GameManager.Instance.GameStatus = menuFlag ? GameManager.GameState.PLAYING : GameManager.GameState.STOP;
+        menuFlag = menuFlag ? false : true;
+    }
+
     public void StartClock()
     {
         Time.timeScale = 1;
