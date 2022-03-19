6985124cf7adc483c6cfda8e2e4dd40dbb21250d
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-03-19 23:40:22 +0900

    アニメーションを変更する処理をクラスで作成した

---
+++ Assets/IKController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IKController : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("左ひざのHint用Transform")]
+    private Transform leftKneeTransform;
+
+    [SerializeField]
+    [Tooltip("右ひざのHint用Transform")]
+    private Transform rightKneeTransform;
+
+    private Animator m_anim = default;
+
+    private float IKValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IKValue = 1;
+        m_anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void OnAnimatorIK(int layerIndex)
+    {
+        m_anim.SetIKHintPositionWeight(AvatarIKHint.RightKnee, IKValue);
+        m_anim.SetIKHintPositionWeight(AvatarIKHint.LeftKnee, IKValue);
+        m_anim.SetIKHintPosition(AvatarIKHint.RightKnee, rightKneeTransform.position);
+        m_anim.SetIKHintPosition(AvatarIKHint.LeftKnee, leftKneeTransform.position);
+    }
+
+    public void SetIKValue(float weight)
+    {
+        if (weight < 0 || weight > 1) IKValue = 0;
+        IKValue = weight;
+    }
+}
