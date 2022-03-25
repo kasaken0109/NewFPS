df77bbf899b91184a9f5d79f121f8b3bc4da234e
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-03-25 22:58:20 +0900

    パッシブスキルを定義するクラスを作成した。

---
+++ Assets/Scripts/PassiveSkill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "PassiveSkill")]
+public class PassiveSkill:ScriptableObject
+{
+    [SerializeField]
+    [Tooltip("適用する効果の種類")]
+    private PassiveType passiveType = default;
+
+    [SerializeField]
+    [Tooltip("適用される効果量")]
+    [Range(-1, 1)]
+    private float effectAmount = 0; 
+}
+
+public enum PassiveType
+{
+    AttackBuf,
+    DefenceBuf,
+    SpeedBuf,
+    Stancebuf,
+    AddEffect,
+}
