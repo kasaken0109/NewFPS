b131d6b5df3028d2b8fd0e4d9743e57b998378d4
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-04-08 23:47:28 +0900

    浮遊状態の時の挙動を定義した

--- Assets/DroneController.cs
+++ Assets/DroneController.cs
@@ -20,23 +20,35 @@ public class DroneController : MonoBehaviour
     [SerializeField]
     [Tooltip("回転時間")]
     private float m_chaseLookTime = 0.5f;
+    [SerializeField]
+    [Tooltip("浮遊の高さ")]
+    private float m_floatHeight = 0.7f;
 
     private float distance;
 
     private GameObject m_attackTarget;
 
+    private DroneMode mode;
+
     // Start is called before the first frame update
     void Start()
     {
         m_attackTarget = GameObject.FindGameObjectWithTag("Enemy");
+        mode = DroneMode.Normal;
     }
 
     // Update is called once per frame
     void Update()
     {
         LookEnemy();
+        transform.position = new Vector3(transform.position.x, m_chaseTarget.transform.position.y + m_floatHeight, transform.position.z);
         distance = Vector3.Distance(transform.position,m_chaseTarget.transform.position);
         if (distance >= m_startChaseDistance) Chase();
+        if(mode == DroneMode.Floating)
+        {
+            transform.position = m_chaseTarget.transform.position + m_chaseOffset;
+            transform.rotation = Quaternion.LookRotation(m_chaseTarget.transform.forward);
+        }
     }
 
     private void Chase()
@@ -51,5 +63,16 @@ public class DroneController : MonoBehaviour
         Vector3 diff = m_attackTarget.transform.position - transform.position;
         Quaternion lookAngle = Quaternion.LookRotation(diff);
         transform.rotation = Quaternion.Slerp(transform.rotation, lookAngle,m_chaseLookTime *Time.deltaTime);
+    }
+
+    public enum DroneMode
+    {
+        Normal,
+        Floating,
+    }
+
+    public void SetMode(int droneMode)
+    {
+        mode = (DroneMode)droneMode;
     }
 }
