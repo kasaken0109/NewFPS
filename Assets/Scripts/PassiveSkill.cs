546a6e76148960435eb090faee1664259a8b73a6
DESKTOP-RE8KB50\vantan <kengokasusai@gmail.com>
2022-05-09 23:33:08 +0900

    パッシブスキルのパラメーターを追加した

--- Assets/Scripts/PassiveSkill.cs
+++ Assets/Scripts/PassiveSkill.cs
@@ -11,9 +11,42 @@ public class PassiveSkill:ScriptableObject
     private PassiveType passiveType = default;
 
     [SerializeField]
+    [Tooltip("効果時間")]
+    private float effectableTime = 5f;
+
+    [SerializeField]
     [Tooltip("適用される効果量")]
     [Range(-1, 1)]
-    private float effectAmount = 0; 
+    private float effectAmount = 0;
+
+    [SerializeField]
+    [Tooltip("消費コスト")]
+    [Range(-1, 1)]
+    private float consumeCost = 0.2f;
+
+    [SerializeField]
+    [Tooltip("説明文")]
+    private string explainText = default;
+
+    [SerializeField]
+    private string skillName = default;
+
+    [SerializeField]
+    [Tooltip("")]
+    private Sprite image = default;
+    public PassiveType PassiveType => passiveType;
+
+    public float EffectableTime => effectableTime;
+
+    public float EffectAmount => effectAmount;
+
+    public float ConsumeCost => consumeCost;
+
+    public string ExplainText => explainText;
+
+    public Sprite ImageBullet => image;
+
+    public string SkillName => skillName;
 }
 
 public enum PassiveType
@@ -21,6 +54,7 @@ public enum PassiveType
     AttackBuf,
     DefenceBuf,
     SpeedBuf,
+    HealBuf,
     Stancebuf,
     AddEffect,
 }
